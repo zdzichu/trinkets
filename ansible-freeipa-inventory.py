5e6a008d526b4bdf7d885a49ddde38795d332346
Tomasz Torcz <tomek@pipebreaker.pl>
2013-12-14 14:47:52 +0100

    implement --list and --host switches

--- ansible-freeipa-inventory.py
+++ ansible-freeipa-inventory.py
@@ -9,10 +9,46 @@
 from ipalib import api
 from ipalib import errors
 
-api.bootstrap(context='iparest')
-api.finalize()
+import json
+import sys
 
-api.Backend.xmlclient.connect()
 
-print api.Command['host_find']()['result']
+def groups_find(api):
+	inventory = {}
+	inventory["ungrouped"] = { "hosts" : [] }
 
+	# TODO ask only for fqdn and memberof_hostgroup (attrlist?)
+	for host in api.Command["host_find"]()["result"]:
+		host_fqdn = host["fqdn"][0]
+
+		try:
+			for group in host['memberof_hostgroup']:
+
+				if not group in inventory:
+					# first occurence of this group
+					inventory[group] = { "hosts" : [] }
+
+				inventory[group]["hosts"].append(host_fqdn)
+		except KeyError:
+			# no groups
+			inventory["ungrouped"]["hosts"].append(host_fqdn)
+
+	print json.dumps(inventory)
+
+
+def host_find(api, host):
+	# we have no specific to pass
+	host_vars = {}
+	print json.dumps(host_vars)
+
+
+if __name__ == "__main__":
+	api.bootstrap(context='iparest')
+	api.finalize()
+
+	api.Backend.xmlclient.connect()
+
+	if len(sys.argv) == 2 and (sys.argv[1] == "--list"):
+		groups_find(api)
+	elif len(sys.argv) == 3 and (sys.argv[1] == "--host"):
+		host_find(api, sys.argv[2])
