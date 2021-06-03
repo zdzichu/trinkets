6288db80f1d5e3060b542c6ade5a926b3aecede2
Tomasz Torcz <tomek@pipebreaker.pl>
2021-06-03 12:19:56 +0200

    ssdhealth: basic collector script

---
+++ ssd-health-dashboard/collector/store-smart.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+R_TTL=$((7*24*3600))
+
+HOST=$(hostname --fqdn)
+
+for drive in /dev/sd? /dev/nvme?; do
+	R_KEY="ssdead/${HOST}/$(basename $drive)"
+	echo Storing $R_KEY
+	smartctl --all --json=c $drive | \
+		redis-cli -h $R_HOST -p $R_PORT -x set $R_KEY
+	redis-cli -h $R_HOST -p $R_PORT expire $R_KEY $R_TTL
+done
