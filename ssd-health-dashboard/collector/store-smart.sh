ff75faf0bff9835d5d2e3106ab4e2669bafbc87a
Tomasz Torcz <tomek@pipebreaker.pl>
2021-06-04 16:10:35 +0200

    ssdhealth: skip non-existing device nodes

--- ssd-health-dashboard/collector/store-smart.sh
+++ ssd-health-dashboard/collector/store-smart.sh
@@ -5,6 +5,7 @@ R_TTL=$((7*24*3600))
 HOST=$(hostname --fqdn)
 
 for drive in /dev/sd? /dev/nvme?; do
+	[ -e $drive ] || continue
 	R_KEY="ssdead/${HOST}/$(basename $drive)"
 	echo Storing $R_KEY
 	smartctl --all --json=c $drive | \
