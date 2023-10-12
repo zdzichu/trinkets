0a0da7e26bbe52f8778f101eeaa28276872c70a5
Tomasz Torcz <tomek@pipebreaker.pl>
2023-10-12 08:03:30 +0200

    logtemp: schema for humidity

--- logtemp/schema.sql
+++ logtemp/schema.sql
@@ -12,9 +12,21 @@ CREATE TABLE temperatures (
 	value	double precision
 );
 
+CREATE TABLE humidities (
+	id	bigserial PRIMARY KEY,
+	datetime	timestamp with time zone NOT NULL DEFAULT NOW(),
+	sensor_id	int NOT NULL,
+	value	double precision
+);
+
 CREATE INDEX idx_temperatures_timestamp ON temperatures USING BRIN(datetime);
 -- help grafana using extract epoch - and not getting index goodies
 -- see also https://www.depesz.com/2014/04/04/how-to-deal-with-timestamps/
 CREATE INDEX idx_temperatures_timestamp_brin ON temperatures((extract(epoch from datetime at time zone 'UTC')));
 
 CREATE INDEX idx_sensors ON temperatures(sensor_id);
+
+
+CREATE INDEX idx_humidities_timestamp_brin ON humidities USING BRIN(datetime);
+CREATE INDEX idx_humidities_timestamp ON humidities((extract(epoch from datetime at time zone 'UTC')));
+CREATE INDEX idx_humidities_sensors ON humidities(sensor_id);
